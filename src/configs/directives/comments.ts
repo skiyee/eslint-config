f4e839adaf9fd618ef976056fc9d059861fddcac
skiyee <319619193@qq.com>
2026-06-26 16:04:47 +0800

    refactor: 支持直接禁用 eslint

--- src/configs/directives/comments.ts
+++ src/configs/directives/comments.ts
@@ -16,7 +16,6 @@ export async function comments(): Promise<FlatConfigItem[]> {
       rules: {
         'eslint-comments/no-aggregating-enable': 'error',
         'eslint-comments/no-duplicate-disable': 'error',
-        'eslint-comments/no-unlimited-disable': 'error',
         'eslint-comments/no-unused-enable': 'error',
       },
     },
